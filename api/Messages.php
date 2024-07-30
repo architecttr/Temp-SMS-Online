2db3a9eaa596fd916d6bb6acde2ca987f551196c
Architect <spymst@gmail.com>
2024-07-30 14:14:01 +0300

    Create Messages.php

---
+++ api/Messages.php
@@ -0,0 +1,17 @@
+<?php
+class Messages {
+    private static $apiUrl = 'https://tempsmsonline.com/api/messages.php?no=';
+
+    public static function getMessages($number) {
+        $url = self::$apiUrl . urlencode($number);
+        $response = file_get_contents($url);
+        if ($response === FALSE) {
+            http_response_code(500);
+            echo json_encode(["message" => "Failed to fetch messages"]);
+            return;
+        }
+        header('Content-Type: application/json');
+        echo $response;
+    }
+}
+?>
